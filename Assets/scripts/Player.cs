cfd5cff84686e1c8eea9f6579701bd19538b5776
Ben Joldersma <ben@forgottenarts.com>
2015-06-01 09:29:34 -0700

    initial commit.

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public enum Cards
+{
+	Move321,
+	Move642,
+	RotCW,
+	RotCCW,
+	RotCW2,
+	RotCCW2,
+}
+
+public class Player : MonoBehaviour {
+
+	public int health = 100;
+	public int maxCards = 5;
+	public List<Cards> cards = new List<Cards>();
+	// Values on water and food indexes represent life levels lost or gained.
+	public int[] waterIndex = new int[0, 0, 1, 1, 2, 3, 4, 7];
+	public int[] foodIndex = new int[0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 6, 6, 8, 10, 13];
+	// Values on life index represent movement points at that life level.
+	public int[] lifeIndex = new int[6, 5, 5, 4, 4, 3, 3, 2, 2, 1, 1, 0, 0, 0, 0];
+
+	void ShuffleCards() {
+		var r = new UnityEngine.Random();
+		var values = Enum.GetValues(typeof(Cards));
+		while(cards.Count < maxCards) {
+			cards.Add((Cards)values.GetValue(UnityEngine.Random.Range(0, values.Length)));
+		}
+	}
+
+	void Start() {
+		ShuffleCards();
+	}
+
+	void OnGUI() {
+		GUI.Label(new Rect(20, 20, 100, 100), health.ToString());
+		for(int i = 0; i < cards.Count; i++) {
+			if (GUI.Button(new Rect(i * 220, Screen.height - 100, 200, 50), cards[i].ToString()))
+			{
+				Debug.Log("move 1");
+			}
+		}
+	}
+}
---
+++ Assets/scripts/TileFactory.cs
@@ -0,0 +1,129 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum TerrainType {
+	Grass,
+	Mountains,
+	Forest
+}
+
+public class Map {
+	public static float HutProbability = 0.1;
+	public static float WaterProbability = 0.2;
+	public static float FoodProbability = 0.2;
+
+	public int Width { get; set; }
+	public int Height { get; set; }
+	public TileFactory Game { get; set; }
+
+	public List<Hex> Hexes = new List<Hex>();
+
+	public Map(int width, int height, TileFactory game)
+	{
+		this.Width = width;
+		this.Height = height;
+		this.Game = game;
+	}
+
+	public Hex GetHex(int x, int y)
+	{
+
+	}
+
+	public void Build(int width, int height)
+	{
+		var rand = new Random();
+		var startHeightX = Random.Range(0, 1000);
+		var startHeightZ = Random.Range(0, 1000);
+		var startWoodsX = Random.Range (0, 1000);
+		var startWoodsZ = Random.Range (0, 1000);
+		for (float z = 0; z < Height; z++) {
+			for (float x = 0; x < Width; x++) {
+				float heightCoordX = (startHeightX + x) * Game.sampleScale;
+				float heightCoordZ = (startHeightZ + z) * Game.sampleScale;
+				float woodsCoordX = (startWoodsX + x) * Game.sampleScale;
+				float woodsCoordZ = (startWoodsZ + z) * Game.sampleScale;
+				float heightSample = Mathf.PerlinNoise(heightCoordX, heightCoordZ) * Game.heightScale;
+				float woodSample = Mathf.PerlinNoise(woodsCoordX, woodsCoordZ) * Game.heightScale;
+				Debug.Log(string.Format("{0}, {1}, {2}", heightCoordX, heightCoordZ, heightSample));
+				if (heightSample < Game.heightScale / 3) heightSample = 2;
+				Hex hex = new Hex() {
+					X = x,
+					Z = z,
+					HasHut = Random.value < HutProbability
+				};
+				hex.HasWater = hex.HasHut || Random.value < WaterProbability;
+				hex.HasFood = hex.HasHut || Random.value < FoodProbability;
+				if (heightSample >= 4) {
+					hex.Terrain = TerrainType.Mountains;
+				} else if (woodSample >= 3) {
+					hex.Terrain = TerrainType.Forest;
+				} else {
+					hex.Terrain = TerrainType.Grass;
+				}
+				Hexes.Add(hex);
+			}
+		}
+	}
+	
+	public Vector2 GameToWorld(float x, float z)
+	{
+		return new Vector2(z % 2 == 0 ? x : x + Game.offsetX, z * Game.vert);
+	}
+}
+
+public class Hex {
+	public int X { get; set; }
+	public int Z { get; set; }
+	public TerrainType Terrain { get; set; }
+	public bool HasHut { get; set; }
+	public bool HasWater { get; set; }
+	public bool HasFood { get; set; }
+	public bool HasTrail { get; set; }
+	public Transform Tile { get; set; }
+}
+
+public class TileFactory : MonoBehaviour {
+	public Material grassy;
+	public Material rocky;
+	public Material forest;
+	public Transform tile;
+
+	public float offsetX = 0.5f;
+	public float offsetY = 1f;
+	public float tileHeight = 0.5f;
+	public float sampleScale = 0.213f;
+	public float heightScale = 5.0f;
+
+	static float r = 0.5f;
+	static float h = r * 2;
+	public static float vert = h * 0.86f;
+	static float mapHeight = 20;
+	static float mapWidth = 20;
+
+	// Use this for initialization
+	void Start () {
+		var map = new Map(mapHeight, mapWidth, this);
+		map.Build();
+		foreach(var hex in map.Hexes) {
+			var v2pos = map.GameToWorld(x, z);
+			Transform t = (Transform)Instantiate(tile, new Vector3(v2pos.x, 0, v2pos.y), Quaternion.identity);
+			t.Rotate(90, 0, 0);
+			Material mat;
+			switch(hex.Terrain) {
+			case TerrainType.Grass:
+				mat = grassy;
+				break;
+			case TerrainType.Forest:
+				mat = forest;
+				break;
+			case TerrainType.Mountains:
+				mat = rocky;
+				break;
+			}
+			t.GetComponent<Renderer>().material = mat;
+			hex.Tile = t;
+		}
+	}
+}
